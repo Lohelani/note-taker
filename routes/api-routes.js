dbb92cfe045e509960087e3bce2f38d6540641e5
Lohelani <lohelani.hicks@gmail.com>
2020-11-19 17:04:22 -0500

    json

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,13 +1,38 @@
 const router = require("express").Router();
 const fs = require('fs');
+const util = require('util');
 //assigns generic ids to elements so computer can read which ones you are trying to delete
 const { v4: uuidv4 } = require('uuid');
 // const bodyPars = require('body-parser');
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
 
+let notes = require("../db/db.json")
+
+router.get("/api/notes", (req, res) => {
+    return res.json(notes)
+})
+
+router.post("/api/notes", function(req, res) {
+    let newNotes = req.body;
+  
+    console.log(newNotes);
+  
+    notes.push(newNotes);
+  
+    let note = JSON.stringify(notes)
+
+    writeFileAsync("./db/db.json", note, function(){
+        return res.json(true)
+    })
+
+    // res.json(newNotes);
+
+  });
 router.get("/notes", function (req, res) {
     //save as variable then parse so it shows up on page
-    return fs.readFileSync('db/db.json', 'utf-8')
-    //return as json
+    return readFileAsync('db/db.json', 'utf-8')
+   
 });
 
 router.post("/notes", function (req, res) {
@@ -15,8 +40,8 @@ router.post("/notes", function (req, res) {
     var newText = req.body.text;
     var newNote = { newTitle, newText, id: uuidv4() };
     console.log(newNote);
-    console.log(newTitle);
     
+    return writeFileAsync('db/db.json', JSON.stringify(newNote))
     // (bodyPars.urlencoded({
     //     extended: true
     //   }));
